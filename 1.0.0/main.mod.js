22ac7845c28760e3944e41efe969038c69b37fee
DoraChad <176503791+DoraChad@users.noreply.github.com>
2025-08-21 08:42:40 -0700

    Update main.mod.js

--- 1.0.0/main.mod.js
+++ 1.0.0/main.mod.js
@@ -1,24 +1,11 @@
 import { PolyMod, MixinType } from "https://pml.orangy.cfd/PolyTrackMods/PolyModLoader/0.5.1/PolyModLoader.js";
 
 class nostalgia extends PolyMod { 
-    loadresources = true;
+    taudioURLS = ["https://github.com/DoraChad/nostalgia/blob/main/assets/gathu.flac"];
     init = function(polyModLoader) {
-        polyModLoader.registerFuncMixin("pl", MixinType.INSERT, `{`, `
-        if (loadresources) {
-          ActivePolyModLoader.getMod("nostalgia").loadresources = false;
-          const loadResource = function(e, t) {
-              ml(this, nl, "f").addResource(),
-              ml(this, tl, "m", dl).call(this, t, (t => {
-                  ml(this, nl, "f").loadedResource(),
-                  null == t ? (console.warn('Audio "' + e + '" failed to load'),
-                  ml(this, il, "f").set(e, null)) : ml(this, il, "f").set(e, t)
-              }
-              ))
-          }
-          loadResource("oldmusic", "https://github.com/DoraChad/nostalgia/raw/refs/heads/main/assets/gathu.flac");
-        }
-        `);
-        polyModLoader.registerFuncMixin("pl", MixinType.REPLACE, `const e = this.getBuffer("music");`, `const e = this.getBuffer("oldmusic");`);
+        polyModLoader.registerClassMixin("gl.prototype", "load", MixinType.INSERT, '{', `ActivePolyModLoader.getMod("nostalgia").audioURLS.forEach(t => {
+            console.log(t);        
+        })`);
     };
 }
 
