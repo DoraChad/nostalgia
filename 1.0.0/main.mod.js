0cfc288731a9b1d08cb919ab0b370a9cc54919b1
DoraChad <176503791+DoraChad@users.noreply.github.com>
2025-08-21 08:42:46 -0700

    Update main.mod.js

--- 1.0.0/main.mod.js
+++ 1.0.0/main.mod.js
@@ -1,7 +1,7 @@
 import { PolyMod, MixinType } from "https://pml.orangy.cfd/PolyTrackMods/PolyModLoader/0.5.1/PolyModLoader.js";
 
 class nostalgia extends PolyMod { 
-    taudioURLS = ["https://github.com/DoraChad/nostalgia/blob/main/assets/gathu.flac"];
+    audioURLS = ["https://github.com/DoraChad/nostalgia/blob/main/assets/gathu.flac"];
     init = function(polyModLoader) {
         polyModLoader.registerClassMixin("gl.prototype", "load", MixinType.INSERT, '{', `ActivePolyModLoader.getMod("nostalgia").audioURLS.forEach(t => {
             console.log(t);        
